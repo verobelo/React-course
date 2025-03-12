9959edd189a2d1e30065a7e2207f463ea367dbfc
verobelo <142001535+verobelo@users.noreply.github.com>
2025-03-12 13:51:44 +0100

    Add state for the position

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -1,19 +1,24 @@
-import './App.css'
-import { useEffect, useState } from 'react'
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App () {
-  const [enabled, setEnabled] = useState(false)
+function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
   useEffect(() => {
-    console.log('Effect: ', { enabled })
-  })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+    }
+    if (enabled) {
+    window.addEventListener("pointermove", handleMove)}
+  });
 
   const handleButtonClick = () => {
-    setEnabled(!enabled)
+    setEnabled(!enabled);
   }
 
   return (
-    <main>
-      <div style={{
+  <main>
+  <div style={{
         position: 'absolute',
         backgroundColor: 'hotpink',
         border: '1px solid #fff',
@@ -24,12 +29,12 @@ function App () {
         top: -25,
         width: 50,
         height: 50,
-        transform: 'translate(0px, 0px)'
+        transform: `translate(0px, 0px)`,
       }}
       />
-      <button onClick={handleButtonClick}>{enabled ? 'Disable ' : 'Enable '}Mouse Follower</button>
-    </main>
-  )
+  <button onClick={handleButtonClick}>{enabled ? "Disable " : "Enable "}Mouse Follower</button>
+  </main>
+  );
 }
 
-export default App
+export default App;
