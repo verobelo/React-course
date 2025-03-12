eee8c52d216deafc61cc194a4e99cd92f3a71bd4
verobelo <142001535+verobelo@users.noreply.github.com>
2025-03-12 13:39:05 +0100

    Add pointer style

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -1,7 +1,35 @@
-import "./App.css";
+import './App.css'
+import { useEffect, useState } from 'react'
 
-function App() {
-  return <h3>Mouse Follower</h3>;
+function App () {
+  const [enabled, setEnabled] = useState(false)
+  useEffect(() => {
+    console.log('Effect: ', { enabled })
+  })
+
+  const handleButtonClick = () => {
+    setEnabled(!enabled)
+  }
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'hotpink',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: 'translate(0px, 0px)'
+      }}
+      />
+      <button onClick={handleButtonClick}>{enabled ? 'Disable ' : 'Enable '}Mouse Follower</button>
+    </main>
+  )
 }
 
-export default App;
+export default App
