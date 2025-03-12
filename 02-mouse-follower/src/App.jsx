1bf52133915dd8e9ac642b4c100c4f12fc256d50
verobelo <142001535+verobelo@users.noreply.github.com>
2025-03-12 19:15:20 +0100

    Add FollowMouse() logic

--- 02-mouse-follower/src/App.jsx
+++ 02-mouse-follower/src/App.jsx
@@ -1,26 +1,50 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [enabled, setEnabled] = useState(false);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  // pointer move
   useEffect(() => {
-    const handleMove = (event) => {
-      const { clientX, clientY } = event;
-    }
-    if (enabled) {
-    window.addEventListener("pointermove", handleMove)}
-  });
+    console.log('effect ', { enabled })
 
-  const handleButtonClick = () => {
-    setEnabled(!enabled);
-  }
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return () => { // cleanup method
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
+
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
 
   return (
-  <main>
-  <div style={{
+    <>
+      <div style={{
         position: 'absolute',
-        backgroundColor: 'hotpink',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
         border: '1px solid #fff',
         borderRadius: '50%',
         opacity: 0.8,
@@ -29,12 +53,22 @@ function App() {
         top: -25,
         width: 50,
         height: 50,
-        transform: `translate(0px, 0px)`,
+        transform: `translate(${position.x}px, ${position.y}px)`
       }}
       />
-  <button onClick={handleButtonClick}>{enabled ? "Disable " : "Enable "}Mouse Follower</button>
-  </main>
-  );
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Disable ' : 'Enable' } follower
+      </button>
+    </>
+  )
 }
 
-export default App;
+function App () {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
